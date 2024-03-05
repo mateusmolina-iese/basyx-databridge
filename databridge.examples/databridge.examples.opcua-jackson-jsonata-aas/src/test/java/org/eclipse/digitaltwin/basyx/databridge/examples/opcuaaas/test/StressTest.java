d75610db6ae4921b8dd3c2f5bef237ccf5106a22
Mateus Molina <mateus.molina@iese.fraunhofer.de>
2024-03-05 09:56:32 +0100

    Implement Stress Test #3 for OPC UA Module

--- databridge.examples/databridge.examples.opcua-jackson-jsonata-aas/src/test/java/org/eclipse/digitaltwin/basyx/databridge/examples/opcuaaas/test/StressTest.java
+++ databridge.examples/databridge.examples.opcua-jackson-jsonata-aas/src/test/java/org/eclipse/digitaltwin/basyx/databridge/examples/opcuaaas/test/StressTest.java
@@ -26,17 +26,18 @@ package org.eclipse.digitaltwin.basyx.databridge.examples.opcuaaas.test;
 
 import static org.junit.Assert.assertNotEquals;
 
-import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
-import org.eclipse.basyx.aas.metamodel.connected.ConnectedAssetAdministrationShell;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.basyx.aas.metamodel.map.descriptor.CustomId;
-import org.eclipse.basyx.aas.registration.memory.InMemoryRegistry;
-import org.eclipse.basyx.components.aas.AASServerComponent;
-import org.eclipse.basyx.components.aas.configuration.AASServerBackend;
-import org.eclipse.basyx.components.aas.configuration.BaSyxAASServerConfiguration;
-import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
-import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
 import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
-import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
 import org.eclipse.digitaltwin.basyx.databridge.aas.configuration.factory.AASProducerDefaultConfigurationFactory;
 import org.eclipse.digitaltwin.basyx.databridge.core.component.DataBridgeComponent;
 import org.eclipse.digitaltwin.basyx.databridge.core.configuration.factory.RoutesConfigurationFactory;
@@ -44,100 +45,103 @@ import org.eclipse.digitaltwin.basyx.databridge.core.configuration.route.core.Ro
 import org.eclipse.digitaltwin.basyx.databridge.jsonata.configuration.factory.JsonataDefaultConfigurationFactory;
 import org.eclipse.digitaltwin.basyx.databridge.jsonjackson.configuration.factory.JsonJacksonDefaultConfigurationFactory;
 import org.eclipse.digitaltwin.basyx.databridge.opcua.configuration.factory.OpcuaDefaultConfigurationFactory;
-import org.eclipse.milo.examples.server.ExampleServer;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Based on TestAASUpdater
+ * 
+ * Requires running MongoDB and BaSyxV2 instances
+ * 
+ * @see docker-compose.yaml @ src/test/resources
  */
 public class StressTest {
-	private static AASServerComponent aasServer;
 	private static DataBridgeComponent updater;
-	private static InMemoryRegistry registry;
-	protected static ExampleServer opcUaServer;
 
-	protected static IIdentifier deviceAAS = new CustomId("TestUpdatedDeviceAAS");
-	private static BaSyxContextConfiguration aasContextConfig;
+	protected static IIdentifier deviceAAS = new CustomId("https://example.com/ids/sm/0002_0122_2042_5943");
 
-	private final static long TOTAL_TEST_TIME = 60 * 1000 * 5; // ms
-	private final static long PROPERTY_CHECK_INTERVAL = 500; // ms
+	private final static long TOTAL_TEST_TIME = 60 * 1000 * 6; // ms
+	private final static long PROPERTY_CHECK_INTERVAL = 2000; // ms
+
+	private static final String SUBMODEL_ENDPOINT = "http://localhost:8081/submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vMDAwMl8wMTIyXzIwNDJfNjI1Mw";
+	private static final String[] ID_SHORTS = { "DynamicFloat" };
 
 	private final static int NUMBER_OF_CHECKS = (int) (TOTAL_TEST_TIME / PROPERTY_CHECK_INTERVAL);
+	private Object[] lastValues = new Object[ID_SHORTS.length];
 
-	private Object propALastValue = "";
-
-	@BeforeClass
-	public static void setUp() throws Exception {
-		System.out.println("Setting up env...");
-		registry = new InMemoryRegistry();
-
-		aasContextConfig = new BaSyxContextConfiguration(4001, "");
-		BaSyxAASServerConfiguration aasConfig = new BaSyxAASServerConfiguration(AASServerBackend.INMEMORY,
-				"aasx/updatertest.aasx");
-		aasServer = new AASServerComponent(aasContextConfig, aasConfig);
-		aasServer.setRegistry(registry);
-	}
+	private static CloseableHttpClient httpClient = HttpClients.createDefault();
 
 	@AfterClass
 	public static void tearDown() throws Exception {
 		System.out.println("Tearing down env...");
 		updater.stopComponent();
-		aasServer.stopComponent();
 	}
 
 	@Test
 	public void test() throws Exception {
-		aasServer.startComponent();
-		System.out.println("AAS STARTED");
-		System.out.println("START UPDATER");
-		ClassLoader loader = this.getClass().getClassLoader();
-		RoutesConfiguration configuration = new RoutesConfiguration();
-
-		// Extend configutation for connections
-		RoutesConfigurationFactory routesFactory = new RoutesConfigurationFactory(loader);
-		configuration.addRoutes(routesFactory.create());
-
-		// Extend configutation for Opcua Source
-		OpcuaDefaultConfigurationFactory opcuaConfigFactory = new OpcuaDefaultConfigurationFactory(loader);
-		configuration.addDatasources(opcuaConfigFactory.create());
-
-		// Extend configuration for AAS
-		AASProducerDefaultConfigurationFactory aasConfigFactory = new AASProducerDefaultConfigurationFactory(loader);
-		configuration.addDatasinks(aasConfigFactory.create());
-
-		JsonJacksonDefaultConfigurationFactory jsonJacksonConfigFactory = new JsonJacksonDefaultConfigurationFactory(
-				loader);
-		configuration.addTransformers(jsonJacksonConfigFactory.create());
-
-		// Extend configuration for Jsonata
-		JsonataDefaultConfigurationFactory jsonataConfigFactory = new JsonataDefaultConfigurationFactory(loader);
-		configuration.addTransformers(jsonataConfigFactory.create());
-
-		updater = new DataBridgeComponent(configuration);
-		updater.startComponent();
+		setupDatabridge();
 
 		Thread.sleep(3000);
+
+		List<String> propEndpoints = Arrays.asList(ID_SHORTS).stream().map(StressTest::buildGetPropValueEndpoint).collect(Collectors.toList());
 
 		for (int i = 0; i < NUMBER_OF_CHECKS; i++) {
 			Thread.sleep(PROPERTY_CHECK_INTERVAL);
 			long elapsedTime = i * PROPERTY_CHECK_INTERVAL / 1000; // s
 			System.out.println("Try=#" + i + " / ElapsedTime=" + elapsedTime + "s");
-			checkProperty();
+			checkProperties(propEndpoints);
 		}
 	}
 
+	private void checkProperties(List<String> endpoints) throws InterruptedException, IOException {
+		int i = 0;
+		for (String propEndpoint : endpoints) {
+			String result = executeHttpGetRequest(propEndpoint);
+			System.out.println(result);
 
-	private void checkProperty() throws InterruptedException {
-		ConnectedAssetAdministrationShellManager manager = new ConnectedAssetAdministrationShellManager(registry);
-		ConnectedAssetAdministrationShell aas = manager.retrieveAAS(deviceAAS);
-		ISubmodel sm = aas.getSubmodels().get("ConnectedSubmodel");
-		ISubmodelElement propertyA = sm.getSubmodelElement("ConnectedPropertyA");
-		Object propAValue = propertyA.getValue();
+			assertNotEquals(lastValues[i], result); // Shouldn't be the case if the connection fails
 
-		// Should not be the case when the connection times out
-		assertNotEquals(propALastValue, propAValue);
-		propALastValue = propAValue;
+			lastValues[i] = result;
+			i++;
+		}
 	}
+
+	private static String buildGetPropValueEndpoint(String idShort) {
+		return SUBMODEL_ENDPOINT + "/submodel-elements/" + idShort + "/$value";
+	}
+
+	private void setupDatabridge() {
+		ClassLoader loader = this.getClass().getClassLoader();
+		RoutesConfiguration configuration = new RoutesConfiguration();
+
+		RoutesConfigurationFactory routesFactory = new RoutesConfigurationFactory(loader);
+		configuration.addRoutes(routesFactory.create());
+
+		OpcuaDefaultConfigurationFactory opcuaConfigFactory = new OpcuaDefaultConfigurationFactory(loader);
+		configuration.addDatasources(opcuaConfigFactory.create());
+
+		AASProducerDefaultConfigurationFactory aasConfigFactory = new AASProducerDefaultConfigurationFactory(loader);
+		configuration.addDatasinks(aasConfigFactory.create());
+
+		JsonJacksonDefaultConfigurationFactory jsonJacksonConfigFactory = new JsonJacksonDefaultConfigurationFactory(loader);
+		configuration.addTransformers(jsonJacksonConfigFactory.create());
+
+		JsonataDefaultConfigurationFactory jsonataConfigFactory = new JsonataDefaultConfigurationFactory(loader);
+		configuration.addTransformers(jsonataConfigFactory.create());
+
+		updater = new DataBridgeComponent(configuration);
+		updater.startComponent();
+	}
+
+	private static String executeHttpGetRequest(String endpoint) throws IOException {
+		HttpGet request = new HttpGet(endpoint);
+		CloseableHttpResponse response = null;
+		try {
+			response = httpClient.execute(request);
+			return EntityUtils.toString(response.getEntity(), "UTF-8");
+		} finally {
+			response.close();
+		}
+	}
+
 }
